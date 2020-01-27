17917f8a7d549454c8d0db5677a829ce12d1fd24
nikola <nikola4work@gmail.com>
2020-01-27 07:13:32 -0500

    installed required packages & created constructor for a manager

---
+++ app.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+//"util" module provides some functions to print formatted strings
+const Manager = "";
+const Engineer = "";
+const Intern = "";
+const html = "";
